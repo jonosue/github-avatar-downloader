ae4a76076617ce5605f2a22c4ec5b3e1b2592021
Jonathan Sue <hello@jonosue.com>
2017-07-31 23:41:13 +0000

    Added a URL request that pushes through a User-Agent header as well as our request URL. Then I logged the contents of the body of the jquery repo to the terminal.

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,29 @@
 var request = require('request');
+var fs = require('fs');
 
 var GITHUB_USER = "jonosue";
 var GITHUB_TOKEN = "a3efe2b10af74e29f9ccd2b8e587117f553f562d";
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+
+  var options = {
+    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+
+  request(options, function(err, response, body) {
+    if (err) throw err;
+    console.log(response.body);
+});
+
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+
