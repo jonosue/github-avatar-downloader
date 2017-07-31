d23021734aa411e6d59337922941ccc2db9e912e
Jonathan Sue <hello@jonosue.com>
2017-07-31 21:49:48 +0000

    Added requestURL variable to main code file, which concatenates the GitHub user and access token with the repo owner and repo name parameters to return a URL of the contributor list in JSON format.

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,11 @@
 var request = require('request');
 
+var GITHUB_USER = "jonosue";
+var GITHUB_TOKEN = "a3efe2b10af74e29f9ccd2b8e587117f553f562d";
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
