6c4ce39d36bef3d4bcd7fdd57d36758ef6720926
Jonathan Sue <hello@jonosue.com>
2017-08-01 02:02:39 +0000

    Added code which forces users to input two command-line arguments. If they don't, it causes an error and logs instructions to the console.

--- download_avatars.js
+++ download_avatars.js
@@ -12,31 +12,33 @@ var name = process.argv[3];
 
 // This function accepts two arguments related to the repo, and other for a callback function which works with the results of the JSON parse.
 function getRepoContributors(repoOwner, repoName, cb) {
-
+// Added logic to enforce command-line arguments. If the two arguments are not passed, then the program fails. Otherwise, it proceeds normally.
+  if (!owner || !name) {
+    console.log("USER ERROR. Please enter two arguments. The first should be the GitHub username of the repo owner, and the second should be the repo name. Please note, the repo must be public." + "\n" + "\n")
+  }
+  else {
 // We need to define our URL in this format, in order to pass 'User-Agent' in the headers.
-  var options = {
-    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-    headers: {
-      'User-Agent': 'GitHub Avatar Downloader - Student Project'
-    }
-  };
+    var options = {
+      url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+        headers: {
+          'User-Agent': 'GitHub Avatar Downloader - Student Project'
+        }
+    };
 
 // This is a very simple request that either throws an error or parses the body of the response from our URL. It passes the err and the response to the callback function.
-  request(options, function(err, response, body) {
-    if (err) throw err;
-    cb(err, JSON.parse(response.body));
-  });
-
+    request(options, function(err, response, body) {
+      if (err) throw err;
+      cb(err, JSON.parse(response.body));
+    });
+  }
 };
 
 // This function will accept the avatar URL and the filepath in order to create the images on disk.
 function downloadImageByURL(url, filePath) {
-
   request.get(url)
 // We will throw an error and log it if we experience an issue.
   .on('error', function (err) {
     throw err;
-    console.log('There was an error with your request.')
   })
 // Let's tell the user as each image finishes downloading to our local file system.
   .on('end', function() {
@@ -51,7 +53,7 @@ function downloadImageByURL(url, filePath) {
 getRepoContributors(owner, name, function(err, result) {
 // Throw an error if there is an issue.
   if (err) {
-    console.log(err);
+    console.log('There was an error with your request. Please ensure that you have spelled the user name and repo name correctly.')
   }
   else {
     for (var x in result) {
