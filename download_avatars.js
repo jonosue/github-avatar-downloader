e26017fa997aa31e44f65f9b3f2fad496c7aeb79
Jonathan Sue <hello@jonosue.com>
2017-08-01 01:43:32 +0000

    Added support for users to enter command-line arguments for the repo owner and repo name that they wish to download contributor images from.

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,19 @@
+// We need to require the request module, as well as the file system module (since we will be writing images to the disk)
 var request = require('request');
 var fs = require('fs');
 
+// Although it may be wise to conceal/obscure the GitHub API token, I have not done this for the purposes of this exercise.
 var GITHUB_USER = "jonosue";
 var GITHUB_TOKEN = "a3efe2b10af74e29f9ccd2b8e587117f553f562d";
 
+// This allows the user to pass command-line arguments for the GitHub repo owner as well as the GitHub repo name to avoid hard-coding values.
+var owner = process.argv[2];
+var name = process.argv[3];
 
+// This function accepts two arguments related to the repo, and other for a callback function which works with the results of the JSON parse.
 function getRepoContributors(repoOwner, repoName, cb) {
 
+// We need to define our URL in this format, in order to pass 'User-Agent' in the headers.
   var options = {
     url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
@@ -14,34 +21,35 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
+// This is a very simple request that either throws an error or parses the body of the response from our URL. It passes the err and the response to the callback function.
   request(options, function(err, response, body) {
     if (err) throw err;
     cb(err, JSON.parse(response.body));
-
   });
 
 };
 
-
+// This function will accept the avatar URL and the filepath in order to create the images on disk.
 function downloadImageByURL(url, filePath) {
 
   request.get(url)
-
+// We will throw an error and log it if we experience an issue.
   .on('error', function (err) {
     throw err;
     console.log('There was an error with your request.')
   })
-
+// Let's tell the user as each image finishes downloading to our local file system.
   .on('end', function() {
     console.log('Image download complete.');
   })
-
+// This downloads the image to the file path we specify.
   .pipe(fs.createWriteStream(filePath));
 
 };
 
-
-getRepoContributors("jquery", "jquery", function(err, result) {
+// This invokes the function and passes the command line arguments as parameters. The callback function iterates through the JSON result and downloads each image one by one.
+getRepoContributors(owner, name, function(err, result) {
+// Throw an error if there is an issue.
   if (err) {
     console.log(err);
   }
@@ -49,6 +57,7 @@ getRepoContributors("jquery", "jquery", function(err, result) {
     for (var x in result) {
       var avatarURL = result[x].avatar_url;
       var downloadPath = './avatars/' + result[x].login + '.jpg';
+// Execute the downloadImageByURL function for each iteration for the "for..in" loop
       downloadImageByURL(avatarURL, downloadPath);
     }
   }
